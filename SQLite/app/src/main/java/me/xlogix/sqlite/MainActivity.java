1d96482fab5a0d56491cf67a837ca0bca905a7f9
Uniyal <abhishekuniyal09@gmail.com>
2017-12-13 20:14:35 +0530

    Add SQLite integration

---
+++ SQLite/app/src/main/java/me/xlogix/sqlite/MainActivity.java
@@ -0,0 +1,40 @@
+package me.xlogix.sqlite;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        DatabaseHandler db = new DatabaseHandler(this);
+        
+        /**
+         * CRUD Operations (Create, Read, Update and Delete)
+         * */
+        // Inserting Contacts
+        Log.d("Insert: ", "Inserting ..");
+        db.addContact(new Contact("Abhishek", "9100000000"));
+        db.addContact(new Contact("Abhi", "9199999999"));
+        db.addContact(new Contact("xlogix", "9522222222"));
+        db.addContact(new Contact("Uniyal", "9533333333"));
+ 
+        // Reading all contacts
+        Log.d("Reading: ", "Reading all contacts..");
+        List<Contact> contacts = db.getAllContacts();       
+ 
+        for (Contact cn : contacts) {
+            String log = "Id: "+cn.getID()+" ,Name: " + cn.getName() + " ,Phone: " + cn.getPhoneNumber();
+                // Writing Contacts to log
+        Log.d("Name: ", log);
+        
+        }
+    }
+}
